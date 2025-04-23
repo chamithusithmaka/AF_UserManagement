e66821393c4a4b986fa726adbe3925f6e0c590fe
chamithusithmaka <IT22103918@my.sliit.lk>
2025-04-23 11:21:23 +0530

    protect middleware used

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,13 +1,17 @@
 // backend/routes/authRoutes.js
 const express = require('express');
 const { registerUser, loginUser, updateProfile, changePassword, deleteAccount} = require('../controllers/authController');
+const protect = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
+// Public routes (no authentication required)
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.put('/profile', updateProfile);
-router.put('/password', changePassword);
-router.delete('/', deleteAccount);
+
+// Protected routes (authentication required)
+router.put('/profile', protect, updateProfile);
+router.put('/password', protect, changePassword);
+router.delete('/', protect, deleteAccount);
 
 module.exports = router;
